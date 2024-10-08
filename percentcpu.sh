03321d45c062cd3e55a2a9d03fed7d3551db1bba
berkro <belko003@gmail.com>
2024-10-08 17:50:29 +0200

    17:50

--- percentcpu.sh
+++ percentcpu.sh
@@ -1,19 +1,28 @@
 #/bin/bash
 
-INFO=$(ps -p 1918 -o pid=,cputime=,etime=)
+INFO=$(ps -p 1857 -o pid=,cputime=,etime=)
 
 CTIME=$(echo $INFO | cut -d " " -f2)
 ETIME=$(echo $INFO | cut -d " " -f3)
 
 CHORA=$(echo $CTIME | cut -d ":" -f1)
 CMIN=$(echo $CTIME | cut -d ":" -f2)
+CSEC=$(echo $CTIME | cut -d ":" -f3)
+
+CSECHORA=$(($CHORA*3600))
+CSECMIN=$(($CMIN*60))
+CSECTOTAL=$(($CSECHORA+$CSECMIN+$CSEC))
+
+EMIN=$(echo $ETIME | cut -d ":" -f1)
+ESEC=$(echo $ETIME | cut -d ":" -f2)
+
+ESECMIN=$(($EMIN*60))
+ESECTOTAL=$(($ESECMIN+$ESEC))
+
+PERCENT=$(($CSECTOTAL*100))
+CPUPERCENT=$(($PERCENT/$ESECTOTAL))
 
 
-EHORA=$(echo $ETIME | cut -d ":" -f1)
-EMIN=$(echo $ETIME | cut -d ":" -f2)
 
-CSECHORA=$CHORA*3600
-CSECMIN=$CMIN*60
-
-echo $CTIME $ETIME $CSECMIN $CSECHORA $EHORA $EMIN
+echo $CTIME $CSECTOTAL $ETIME $ESECTOTAL $CPUPERCENT
 
