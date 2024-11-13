b5272ed318e9febdfe84743380f60a69dab97749
berkro <belko003@gmail.com>
2024-11-13 21:00:41 +0100

    Wed Nov 13 09:00:41 PM CET 2024

--- practicaMP6/crearWS.sh
+++ practicaMP6/crearWS.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+GRUPID= ./crearGrup.sh
 
-aws ec2 run-instances --image-id "ami-05f283f34603d6aed" --instance-type "t2.micro" --network-interfaces '{"AssociatePublicIpAddress":true,"DeviceIndex":0,"Groups":["$GRUPID"]}' '{"CpuCredits":"standard"}' \
-										    --tag-specifications '{"ResourceType":"instance","Tags":[{"Key":"Name","Value":"Windows Server"}]}' --count "1"
+aws ec2 run-instances  --count "1" --image-id "ami-05f283f34603d6aed" --instance-type "t2.micro" --security-groups $GRUPID \
+		       --tag-specifications '{"ResourceType":"instance","Tags":[{"Key":"Name","Value":"Windows Server 2022"}]}' > /dev/null
