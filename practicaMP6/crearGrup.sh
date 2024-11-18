8a8c998569cc82706dbaa2b7f8f95266056597c2
berkro <belko003@gmail.com>
2024-11-18 21:01:28 +0100

    Mon Nov 18 09:01:28 PM CET 2024

--- practicaMP6/crearGrup.sh
+++ practicaMP6/crearGrup.sh
@@ -7,9 +7,9 @@ aws ec2 create-security-group --group-name "$NOMGRUP" --description "$NOMGRUP" >
 
 # Afegir regles al grup
 aws ec2 authorize-security-group-ingress --group-name $NOMGRUP --ip-permissions IpProtocol=tcp,FromPort=3389,ToPort=3389,IpRanges="[{CidrIp=0.0.0.0/0}]" \
-										IpProtocol=tcp,FromPort=22,ToPort=22,IpRanges="[{CidrIp=0.0.0.0/0}]"
-										IpProtocol=tcp,FromPort=53,ToPort=53,IpRanges="[{CidrIp=0.0.0.0/0}]"
-										IpProtocol=tcp,FromPort=80,ToPort=80,IpRanges="[{CidrIp=0.0.0.0/0}]"
+										IpProtocol=tcp,FromPort=22,ToPort=22,IpRanges="[{CidrIp=0.0.0.0/0}]" \
+										IpProtocol=tcp,FromPort=53,ToPort=53,IpRanges="[{CidrIp=0.0.0.0/0}]" \
+										IpProtocol=tcp,FromPort=80,ToPort=80,IpRanges="[{CidrIp=0.0.0.0/0}]" \
 										IpProtocol=tcp,FromPort=443,ToPort=443,IpRanges="[{CidrIp=0.0.0.0/0}]" > /dev/null
 # Obtenir grup ID
 GRUPID=$(aws ec2 describe-security-groups --group-name $NOMGRUP --query "SecurityGroups[0].GroupId" --output text)
--- practicaMP6/crearServers.sh
+++ practicaMP6/crearServers.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 GRUPID= ./crearGrup.sh
-
+echo "$GRUPID"
 # Crear Windows Server
-aws ec2 run-instances --count "1" --image-id "ami-05f283f34603d6aed" --instance-type "t2.micro" --security-groups $GRUPID \
-		      --tag-specifications '{"ResourceType":"instance","Tags":[{"Key":"Name","Value":"Windows Server 2022"}]}' > /dev/null
+./crearWS.sh "$GRUPID"
 
 # Crear Linux Server
-aws ec2 run-instances --count "1" --image-id "ami-064519b8c76274859" --instance-type "t2.micro" --security-groups $GRUPID \
-		      --tag-specifications '{"ResourceType":"instance","Tags":[{"Key":"Name","Value":"Linux Server"}]}' > /dev/null
+./crearLS.sh "$GRUPID"
